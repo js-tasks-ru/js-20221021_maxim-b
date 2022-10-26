c9be6d8fb52918e1791bdb3f70174d14a73eab7d
maxim.b <maxim.b@intensa.ru>
2022-10-26 20:04:59 +0300

    sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,21 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+  const arrCopy = [...arr];
+  arrCopy.sort(function (a, b) {
+    if (param === 'asc') {
+      if (a.slice(0, 1) !== b.slice(0, 1) && a.slice(0, 1).toLowerCase() === b.slice(0, 1).toLowerCase()) {
+        return 1;
+      }
+      return a.localeCompare(b);
+    } else {
+      if (a.slice(0, 1) !== b.slice(0, 1) && a.slice(0, 1).toLowerCase() === b.slice(0, 1).toLowerCase()) {
+        return 1;
+      }
+      return b.localeCompare(a);
+    }
+  });
+  return arrCopy;
 }
