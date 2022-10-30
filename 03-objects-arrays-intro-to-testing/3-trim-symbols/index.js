d4a84b9829d2c2b03145eaf585837a6d10fdc29c
maxim.b <maxim.b@intensa.ru>
2022-10-30 21:06:14 +0300

    trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const stringArray = Array.from(string);
+  if (size === 0) {
+    return "";
+  }
+  if (!size) {
+    return string;
+  }
+  let index = 1;
+  let resultArray = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i] === stringArray[i + 1]) {
+      if (index++ < size) {
+        resultArray.push(stringArray[i]);
+      }
+    } else {
+      index = 1;
+      resultArray.push(stringArray[i]);
+    }
 
+  }
+
+  return resultArray.join("");
 }
