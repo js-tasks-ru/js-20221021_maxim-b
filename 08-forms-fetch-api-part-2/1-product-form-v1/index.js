f68de70ed1f0080d41e27f70d90754d4226c0f35
maxim.b <maxim.b@intensa.ru>
2022-11-22 14:48:55 +0300

    rework ProductForm

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -12,6 +12,7 @@ export default class ProductForm {
   onSubmit = (event) => {
     console.log(event);
     event.preventDefault();
+    this.save();
   };
 
   constructor(productId) {
@@ -27,7 +28,7 @@ export default class ProductForm {
   }
 
   get template() {
-    return `  <div class="product-form">
+    return `<div class="product-form">
     <form data-element="productForm" class="form-grid">
       <div class="form-group form-group__half_left">
         <fieldset>
@@ -41,60 +42,13 @@ export default class ProductForm {
       </div>
       <div class="form-group form-group__wide" data-element="sortable-list-container">
         <label class="form-label">Фото</label>
-        <div data-element="imageListContainer"><ul class="sortable-list"><li class="products-edit__imagelist-item sortable-list__item" style="">
-          <input type="hidden" name="url" value="https://i.imgur.com/MWorX2R.jpg">
-          <input type="hidden" name="source" value="75462242_3746019958756848_838491213769211904_n.jpg">
-          <span>
-        <img src="icon-grab.svg" data-grab-handle="" alt="grab">
-        <img class="sortable-table__cell-img" alt="Image" src="https://i.imgur.com/MWorX2R.jpg">
-        <span>75462242_3746019958756848_838491213769211904_n.jpg</span>
-      </span>
-          <button type="button">
-            <img src="icon-trash.svg" data-delete-handle="" alt="delete">
-          </button></li></ul></div>
+        <div data-element="imageListContainer">
+        </div>
         <button type="button" name="uploadImage" class="button-primary-outline"><span>Загрузить</span></button>
       </div>
       <div class="form-group form-group__half_left">
         <label class="form-label">Категория</label>
-        <select class="form-control" name="subcategory" data-element="subCategory">
-          <option value="progulki-i-detskaya-komnata">Детские товары и игрушки &gt; Прогулки и детская комната</option>
-          <option value="kormlenie-i-gigiena">Детские товары и игрушки &gt; Кормление и гигиена</option>
-          <option value="igrushki-i-razvlecheniya">Детские товары и игрушки &gt; Игрушки и развлечения</option>
-          <option value="aktivniy-otdyh-i-ulitsa">Детские товары и игрушки &gt; Активный отдых и улица</option>
-          <option value="radioupravlyaemye-modeli">Детские товары и игрушки &gt; Радиоуправляемые модели</option>
-          <option value="shkolnye-tovary">Детские товары и игрушки &gt; Школьные товары</option>
-          <option value="noutbuki-i-aksessuary">Компьютерная техника &gt; Ноутбуки и аксессуары</option>
-          <option value="monitory">Компьютерная техника &gt; Мониторы</option>
-          <option value="komplektuyuschie">Компьютерная техника &gt; Комплектующие</option>
-          <option value="setevoe-oborudovanie">Компьютерная техника &gt; Сетевое оборудование</option>
-          <option value="vstraivaemaya-tehnika">Крупная бытовая техника &gt; Встраиваемая техника</option>
-          <option value="stiralnye-mashiny">Крупная бытовая техника &gt; Стиральные машины</option>
-          <option value="sushilnye-mashiny">Крупная бытовая техника &gt; Сушильные машины</option>
-          <option value="holodilniki">Крупная бытовая техника &gt; Холодильники</option>
-          <option value="morozilnye-kamery">Крупная бытовая техника &gt; Морозильные камеры</option>
-          <option value="vinnye-shkafy">Крупная бытовая техника &gt; Винные шкафы</option>
-          <option value="vytyazhki">Крупная бытовая техника &gt; Вытяжки</option>
-          <option value="plity">Крупная бытовая техника &gt; Плиты</option>
-          <option value="posudomoechnye-mashiny">Крупная бытовая техника &gt; Посудомоечные машины</option>
-          <option value="melkaya-bytovaya-tehnika">Крупная бытовая техника &gt; Мелкая бытовая техника</option>
-          <option value="mikrovolnovye-pechi">Крупная бытовая техника &gt; Микроволновые печи</option>
-          <option value="elektroduhovki">Крупная бытовая техника &gt; Электродуховки</option>
-          <option value="uborochnye-mashiny">Крупная бытовая техника &gt; Уборочные машины</option>
-          <option value="paroochistiteli">Крупная бытовая техника &gt; Пароочистители</option>
-          <option value="kulery-i-purifayery">Крупная бытовая техника &gt; Кулеры и пурифайеры</option>
-          <option value="kuhnya">Мелкая бытовая техника &gt; Кухня</option>
-          <option value="bytovye-pribory-dlya-doma">Мелкая бытовая техника &gt; Бытовые приборы для дома</option>
-          <option value="krasota-i-gigiena">Мелкая бытовая техника &gt; Красота и гигиена</option>
-          <option value="lcd-televizory">ТВ и видеотехника &gt; LCD телевизоры</option>
-          <option value="podstavki-i-krepleniya">ТВ и видеотехника &gt; Подставки и крепления</option>
-          <option value="mediapleery">ТВ и видеотехника &gt; Медиаплееры</option>
-          <option value="tv-tyunery">ТВ и видеотехника &gt; ТВ тюнеры</option>
-          <option value="tv-antenny">ТВ и видеотехника &gt; ТВ антенны</option>
-          <option value="3d-ochki">ТВ и видеотехника &gt; 3D очки</option>
-          <option value="ochki-virtualnoy-realnosti">ТВ и видеотехника &gt; Очки виртуальной реальности</option>
-          <option value="proektsionnoe-oborudovanie">ТВ и видеотехника &gt; Проекционное оборудование</option>
-          <option value="videokamery-i-aksessuary">ТВ и видеотехника &gt; Видеокамеры и аксессуары</option>
-          <option value="dvd/blu-ray-pleery">ТВ и видеотехника &gt; DVD/Blu-ray плееры</option>
+        <select class="form-control" name="subcategory">
         </select>
       </div>
       <div class="form-group form-group__half_left form-group__two-col">
@@ -114,8 +68,7 @@ export default class ProductForm {
       <div class="form-group form-group__part-half">
         <label class="form-label">Статус</label>
         <select class="form-control" name="status">
-          <option value="1">Активен</option>
-          <option value="0">Неактивен</option>
+
         </select>
       </div>
       <div class="form-buttons">
@@ -128,7 +81,7 @@ export default class ProductForm {
 `;
   }
 
-  async render() {
+  render() {
     const element = document.createElement('div');
     element.innerHTML = this.template;
     this.element = element.firstElementChild;
@@ -165,20 +118,94 @@ export default class ProductForm {
     this.initCategories();
   }
 
-  initProductData() {
+  async initProductData() {
+    let url = new URL('/api/rest/products', BACKEND_URL);
+    url.searchParams.set('id', this.productId);
+    let product = await fetchJson(url);
+    product = product[0];
+    let {
+      id,
+      title,
+      description,
+      brand,
+      quantity,
+      subcategory,
+      status,
+      characteristics,
+      images,
+      price,
+      rating,
+      discount
+    } = product;
 
+    this.subElements.productForm.title.value = title;
+    this.subElements.productForm.description.innerHTML = description;
+    this.subElements.imageListContainer.innerHTML = this.getImages(images);
+    this.subElements.productForm.price.value = price;
+    this.subElements.productForm.discount.value = discount;
+    this.subElements.productForm.quantity.value = quantity;
+    this.subElements.productForm.status.innerHTML = this.getStatus(status);
   }
 
   async initCategories() {
     const categoriesResponse = await fetchJson(new URL('/api/rest/categories?_sort=weight&_refs=subcategory', BACKEND_URL));
-    let categories = categoriesResponse.map(item => {
+    this.subElements.productForm.subcategory.innerHTML = categoriesResponse.map(item => {
       let subCategoryRes = [];
       for (const subCategory of item.subcategories) {
         subCategoryRes.push(`<option value="${subCategory.id}">${item.title}${escapeHtml(" > ")}${subCategory.title}</option>`);
       }
       return subCategoryRes.join("");
     }).join("");
+  }
 
-    this.subElements.subCategory.innerHTML = categories;
+  destroy() {
+    this.element.remove();
+
+    this.subElements.productForm.removeEventListener('submit', this.onSubmit);
+    this.subElements = {};
+    this.element = null;
+
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  save() {
+
+  }
+
+  getImages(images) {
+    let imagesResult = [];
+    imagesResult.push(`<ul class="sortable-list">`);
+    for (const image of images) {
+      imagesResult.push(`<li class="products-edit__imagelist-item sortable-list__item" style="">
+          <input type="hidden" name="url" value="${image.url}">
+          <input type="hidden" name="source" value="${image.source}">
+          <span>
+        <img src="icon-grab.svg" data-grab-handle="" alt="grab">
+        <img class="sortable-table__cell-img" alt="Image" src="${image.url}">
+        <span>${image.source}</span>
+      </span>
+          <button type="button">
+            <img src="icon-trash.svg" data-delete-handle="" alt="delete">
+          </button></li>`);
+    }
+    imagesResult.push(`</ul>`);
+    return imagesResult.join("");
+  }
+
+  getStatus(status) {
+    const statuses = ["Неактивен", "Активен"];
+
+    return statuses.map((value, index) => {
+      let selected = "";
+      if (status === index) {
+        selected = 'selected="selected"';
+      }
+      return `<option value="${index}" ${selected}>${value}</option>`;
+    }).join("");
   }
 }
